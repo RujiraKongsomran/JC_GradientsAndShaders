8d48cb84d33372cc925cd9d1e738705d651ac810
RujiraKongsomran <rujirabiwberry@gmail.com>
2023-03-04 15:42:38 +0700

    Gradient Brushes

--- app/src/main/java/com/rujirakongsomran/jc_gradientsandshaders/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_gradientsandshaders/MainActivity.kt
@@ -3,13 +3,21 @@ package com.rujirakongsomran.jc_gradientsandshaders
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rujirakongsomran.jc_gradientsandshaders.ui.theme.JC_GradientsAndShadersTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,17 +38,58 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GradientBrushes() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 10.dp)
+    ) {
+        val brush = Brush.horizontalGradient(listOf(Color.Red, Color.Blue))
+        Canvas(
+            modifier = Modifier.size(100.dp),
+            onDraw = {
+                drawRect(brush = brush)
+            }
+        )
+
+        val brush2 = Brush.linearGradient(listOf(Color.Red, Color.Blue))
+        Canvas(
+            modifier = Modifier.size(100.dp),
+            onDraw = {
+                drawRect(brush = brush2)
+            }
+        )
+        val brush3 = Brush.verticalGradient(listOf(Color.Red, Color.Blue))
+        Canvas(
+            modifier = Modifier.size(100.dp),
+            onDraw = {
+                drawRect(brush = brush3)
+            }
+        )
+        val brush4 = Brush.sweepGradient(listOf(Color.Red, Color.Blue))
+        Canvas(
+            modifier = Modifier.size(100.dp),
+            onDraw = {
+                drawRect(brush = brush4)
+            }
+        )
+        val brush5 = Brush.radialGradient(listOf(Color.Red, Color.Blue))
+        Canvas(
+            modifier = Modifier.size(100.dp),
+            onDraw = {
+                drawRect(brush = brush5)
+            }
+        )
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     JC_GradientsAndShadersTheme {
-        Greeting("Android")
+        GradientBrushes()
     }
 }
